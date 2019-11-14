18868c5b101bb7f83172b9f245f353f587685dd5
novilfahlevy <novilfreon@gmail.com>
2019-11-14 20:02:42 +0800

    mengubah struktur database.sql

--- database.sql
+++ database.sql
@@ -2,21 +2,20 @@ CREATE TABLE `pemasok` (
   `id_pemasok` int PRIMARY KEY,
   `nama` text,
   `merk` text,
-  `no_telp` text,
-  `jenis` text
+  `no_telp` text
 );
 
 CREATE TABLE `staff` (
   `id_staff` int PRIMARY KEY,
   `nama` text,
-  `jobdesc` text
+  `bidang` text
 );
 
 CREATE TABLE `barang` (
   `id_barang` int PRIMARY KEY,
   `merk` text,
-  `jenis` text,
-  `quantity` int,
+  `id_jenis` int,
+  `jumlah` int,
   `harga` int,
   `id_pemasok` int,
   `id_staff` int
@@ -27,4 +26,9 @@ CREATE TABLE `users` (
   `nama_lengkap` text,
   `email` text.
   `password` text
+);
+
+CREATE TABLE `jenis_barang` (
+  `id` int PRIMARY KEY,
+  `nama` text
 );
