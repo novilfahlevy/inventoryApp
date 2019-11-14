18c18421d7105cac63fd5ba8a1c045c6738b4140
novilfahlevy <novilfreon@gmail.com>
2019-11-14 08:47:22 +0800

    membuat file model auth

---
+++ src/Models/Auth/RegisterModel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models.Auth;
+
+/**
+ *
+ * @author Novil F
+ */
+public class RegisterModel {
+    
+}
