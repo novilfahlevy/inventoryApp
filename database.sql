9a24c631cbbd215cf4e354d0b6bb9f4fcb0d0f94
novilfahlevy <novilfreon@gmail.com>
2019-11-15 19:23:32 +0800

    merubah struktur database

--- database.sql
+++ database.sql
@@ -1,37 +1,180 @@
-CREATE TABLE `pemasok` (
-  `id_pemasok` int PRIMARY KEY,
-  `nama` text,
-  `merk` text,
-  `no_telp` text
-);
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Nov 2019 pada 12.22
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 7.3.1
 
-CREATE TABLE `staff` (
-  `id_staff` int PRIMARY KEY,
-  `nama` text,
-  `bidang` text
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventaris_barang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
 
 CREATE TABLE `barang` (
-  `id_barang` int PRIMARY KEY,
+  `id_barang` int(11) NOT NULL,
   `kode_barang` text,
   `merk` text,
-  `id_jenis` int,
-  `jumlah` int,
-  `harga` int,
-  `id_pemasok` int,
-  `id_staff` int,
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
+  `id_jenis` int(11) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `id_pemasok` int(11) DEFAULT NULL,
+  `id_staff` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `kode_barang`, `merk`, `id_jenis`, `jumlah`, `harga`, `id_pemasok`, `id_staff`, `created_at`, `updated_at`) VALUES
+(1, 'KP1', 'Water Lemon', 2, 30, 6000, 1, 1, '2019-11-15 11:12:42', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jenis_barang`
+--
+
+CREATE TABLE `jenis_barang` (
+  `id` int(11) NOT NULL,
+  `nama` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `jenis_barang`
+--
+
+INSERT INTO `jenis_barang` (`id`, `nama`) VALUES
+(1, 'Makanan'),
+(2, 'Minuman\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemasok`
+--
+
+CREATE TABLE `pemasok` (
+  `id` int(11) NOT NULL,
+  `nama` text,
+  `no_telp` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pemasok`
+--
+
+INSERT INTO `pemasok` (`id`, `nama`, `no_telp`) VALUES
+(1, 'PT. Gunajaya', '0895339906176');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL,
+  `nama` text,
+  `bidang` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `staff`
+--
+
+INSERT INTO `staff` (`id`, `nama`, `bidang`) VALUES
+(1, 'Eddy Gunawan', 'Checker');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
 
 CREATE TABLE `users` (
-  `id` int PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `nama_lengkap` text,
   `email` text,
   `password` text
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `jenis_barang` (
-  `id` int PRIMARY KEY,
-  `nama` text
-);
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `jenis_barang`
+--
+ALTER TABLE `jenis_barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pemasok`
+--
+ALTER TABLE `pemasok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `pemasok`
+--
+ALTER TABLE `pemasok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
