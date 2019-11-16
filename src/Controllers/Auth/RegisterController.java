15b01e0e3529f70cb0df142c2c5e8ffbc61bfe2f
novilfahlevy <novilfreon@gmail.com>
2019-11-16 20:36:00 +0800

    membuat fitur register

--- src/Controllers/Auth/RegisterController.java
+++ src/Controllers/Auth/RegisterController.java
@@ -5,10 +5,79 @@
  */
 package Controllers.Auth;
 
+import Models.Auth.RegisterModel;
+
+import Views.Auth.*;
+
+import javax.swing.JOptionPane;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Novil F
  */
 public class RegisterController {
     
+    RegisterModel modelRegister = new RegisterModel();
+    
+    public void index(Register form) {
+        String namaLengkap = form.textNamaLengkap.getText();
+        String email = form.textEmail.getText();
+        String password = form.textPassword.getText();
+        String konfirmasiPassword = form.textKonfirmasiPassword.getText();
+        
+        if( validasi(namaLengkap, email, password, konfirmasiPassword) && modelRegister.register(namaLengkap, email, password) ) {
+            JOptionPane.showMessageDialog(null, "Registrasi berhasil.");
+            form.dispose();
+            new Login().setVisible(true);
+        }
+    }
+    
+    public boolean validasi(String namaLengkap, String email, String password, String konfirmasiPassword) {
+        if( namaLengkap.equals("") ) {
+            JOptionPane.showMessageDialog(null, "Mohon isi nama lengkap anda.");
+            return false;
+        }
+        
+        if( email.equals("") ) {
+            JOptionPane.showMessageDialog(null, "Mohon isi email anda.");
+            return false;
+        }
+        
+        if( isEmailNotValid(email) ) {
+            JOptionPane.showMessageDialog(null, "Email tidak valid, mohon masukan email yang tepat.");
+            return false;
+        }
+        
+        if( modelRegister.checkEmail(email) ) {
+            JOptionPane.showMessageDialog(null, "Email sudah terdaftar");
+            return false;
+        }
+        
+        if( password.equals("") ) {
+            JOptionPane.showMessageDialog(null, "Mohon isi password anda.");
+            return false;
+        }
+        
+        if( password.length() < 8 ) {
+            JOptionPane.showMessageDialog(null, "Password minimal harus berisi 8 karakter!");
+            return false;
+        }
+        
+        if( konfirmasiPassword.equals("") ) {
+            JOptionPane.showMessageDialog(null, "Mohon konfirmasi password anda.");
+            return false;
+        }
+        
+        if( !konfirmasiPassword.equals(password) ) {
+            JOptionPane.showMessageDialog(null, "Password tidak cocok! Mohon periksa lagi password anda.");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public boolean isEmailNotValid(String email) {
+        return !Pattern.compile("^(.+)@(.+)$").matcher(email).matches();
+    }
 }
--- src/Models/Auth/RegisterModel.java
+++ src/Models/Auth/RegisterModel.java
@@ -5,10 +5,44 @@
  */
 package Models.Auth;
 
+import Database.Koneksi;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Novil F
  */
 public class RegisterModel {
+    public boolean checkEmail(String email) {
+        boolean emailSudahTerdaftar = false;
+        
+        try {
+            ResultSet result = Koneksi.getKoneksi().createStatement().executeQuery("SELECT id FROM `users` WHERE email = '" + email + "'");
+            emailSudahTerdaftar = result.next();
+        } catch( Exception ex ) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        
+        return emailSudahTerdaftar;
+    }
     
+    public boolean register(String namaLengkap, String email, String password) {
+        boolean registerBerhasil = false;
+        
+        try {
+            PreparedStatement ps = Koneksi.getKoneksi().prepareStatement("INSERT INTO `users` VALUES (?, ?, ?, ?)");
+            
+            ps.setString(1, null);
+            ps.setString(2, namaLengkap);
+            ps.setString(3, email);
+            ps.setString(4, password);
+            
+            registerBerhasil = ps.executeUpdate() > 0;
+        } catch( Exception ex ) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        
+        return registerBerhasil;
+    }
 }
